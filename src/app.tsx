f4bad2791a81bd7fbf4088c5cef192088a2057e1
Wojtek Malarski <w.malarski@gmail.com>
2025-12-30 21:21:47 +0100

    adds daisy ui

--- src/app.tsx
+++ src/app.tsx
@@ -4,6 +4,7 @@ import type { Component } from "solid-js";
 export const App: Component = () => (
   <>
     <h1 class="text-2xl text-red-900">Hello world!!!!</h1>
+    <button class="btn">AA</button>
     <Comp />
   </>
 );
