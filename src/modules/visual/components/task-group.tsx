ed0a35f0873e00758348fa4326bb26a2ed46b872
Wojtek Malarski <w.malarski@gmail.com>
2026-01-10 14:46:58 +0100

    adds drawing line

--- src/modules/visual/components/task-group.tsx
+++ src/modules/visual/components/task-group.tsx
@@ -1,6 +1,6 @@
 import { createWritableMemo } from "@solid-primitives/memo";
 import * as d3 from "d3";
-import { createMemo, createSignal, type Component } from "solid-js";
+import { createMemo, createSignal, Show, type Component } from "solid-js";
 import {
   TASK_HANDLE_SIZE,
   TASK_HANDLE_SIZE_HALF,
@@ -60,20 +60,50 @@ type TaskHandleProps = {
 };
 
 const TaskHandle: Component<TaskHandleProps> = (props) => {
-  const [_rectRef, setRectRef] = createSignal<SVGCircleElement>();
+  const [rectRef, setRectRef] = createSignal<SVGCircleElement>();
 
-  const yShift = createMemo(
+  const xShift = createMemo(
     () => (props.kind === "source" ? TASK_RECT_WIDTH : 0) - TASK_HANDLE_SIZE_HALF,
   );
 
+  const [isDragging, setIsDragging] = createSignal(false);
+  const [x, setX] = createSignal(0);
+  const [y, setY] = createSignal(0);
+
+  createDrag({
+    onDragEnded() {
+      setIsDragging(false);
+    },
+    onDragStarted() {
+      setIsDragging(true);
+    },
+    onDragged(event) {
+      setX(event.x);
+      setY(event.y);
+    },
+    ref: rectRef,
+  });
+
+  const path = createMemo(() => {
+    const context = d3.path();
+    context.moveTo(props.x + xShift(), props.y + TASK_HANDLE_Y_SHIFT);
+    context.lineTo(x(), y());
+    return context.toString();
+  });
+
   return (
-    <rect
-      ref={setRectRef}
-      x={props.x + yShift()}
-      y={props.y + TASK_HANDLE_Y_SHIFT}
-      width={TASK_HANDLE_SIZE}
-      height={TASK_HANDLE_SIZE}
-      fill="blue"
-    />
+    <>
+      <rect
+        ref={setRectRef}
+        x={props.x + xShift()}
+        y={props.y + TASK_HANDLE_Y_SHIFT}
+        width={TASK_HANDLE_SIZE}
+        height={TASK_HANDLE_SIZE}
+        fill="blue"
+      />
+      <Show when={isDragging()}>
+        <path d={path()} stroke="red" />
+      </Show>
+    </>
   );
 };
