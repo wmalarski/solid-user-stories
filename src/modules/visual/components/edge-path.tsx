483ff502e79ccc6a90414bae040cc3487d00c5e0
Wojtek Malarski <w.malarski@gmail.com>
2026-01-11 11:55:10 +0100

    adds edges

---
+++ src/modules/visual/components/edge-path.tsx
@@ -0,0 +1,33 @@
+import * as d3 from "d3";
+import { createMemo, type Component } from "solid-js";
+import type { EdgeModel, TaskModel } from "~/integrations/tanstack-db/schema";
+import { useBoardThemeContext } from "../contexts/board-theme";
+import { TASK_RECT_HEIGHT, TASK_RECT_WIDTH } from "../utils/constants";
+
+type EdgePathProps = {
+  edge: EdgeModel;
+  source: TaskModel;
+  target: TaskModel;
+};
+
+export const EdgePath: Component<EdgePathProps> = (props) => {
+  const boardTheme = useBoardThemeContext();
+
+  const path = createMemo(() => {
+    const startX = props.source.positionX + TASK_RECT_WIDTH;
+    const endX = props.target.positionX;
+
+    const heightOffset = TASK_RECT_HEIGHT / 2;
+    const startY = props.source.positionY + heightOffset;
+    const endY = props.target.positionY + heightOffset;
+
+    const context = d3.path();
+    context.moveTo(startX, startY);
+    context.lineTo(props.edge.breakX, startY);
+    context.lineTo(props.edge.breakX, endY);
+    context.lineTo(endX, endY);
+    return context.toString();
+  });
+
+  return <path d={path()} stroke-width={1} stroke={boardTheme().edgeColor} fill="transparent" />;
+};
--- src/modules/visual/components/visual-panel.tsx
+++ src/modules/visual/components/visual-panel.tsx
@@ -4,11 +4,13 @@ import { AxisConfigProvider } from "../contexts/axis-config";
 import { BoardModelProvider } from "../contexts/board-model";
 import { BoardThemeProvider } from "../contexts/board-theme";
 import { BoardTransformProvider, useBoardTransformContext } from "../contexts/board-transform";
+import { EdgesDataProvider, useEdgesDataContext } from "../contexts/edges-data";
 import { TasksDataProvider, useTasksDataContext } from "../contexts/tasks-data";
 import { useZoomTransform } from "../utils/use-zoom-transform";
 import { AxisGridPaths } from "./axis-grid-paths";
 import { AxisGroup } from "./axis-group";
 import { DragGroup } from "./drag-group";
+import { EdgePath } from "./edge-path";
 import { TaskGroup } from "./task-group";
 
 type VisualPanelProps = {
@@ -21,9 +23,11 @@ export const VisualPanel: Component<VisualPanelProps> = (props) => {
       <BoardTransformProvider>
         <BoardModelProvider board={props.board}>
           <TasksDataProvider>
-            <AxisConfigProvider>
-              <DragAndDropExample />
-            </AxisConfigProvider>
+            <EdgesDataProvider>
+              <AxisConfigProvider>
+                <DragAndDropExample />
+              </AxisConfigProvider>
+            </EdgesDataProvider>
           </TasksDataProvider>
         </BoardModelProvider>
       </BoardTransformProvider>
@@ -38,11 +42,15 @@ const DragAndDropExample: Component = () => {
   useZoomTransform({ ref: svgRef });
 
   const tasksData = useTasksDataContext();
+  const edgesData = useEdgesDataContext();
 
   return (
     <svg ref={setSvgRef} class="w-screen h-screen">
       <AxisGridPaths />
       <DragGroup transform={boardTransformContext().transform() as unknown as string}>
+        <For each={edgesData().entries}>
+          {(entry) => <EdgePath edge={entry.edge} source={entry.source} target={entry.target} />}
+        </For>
         <For each={tasksData().entries}>{(task) => <TaskGroup task={task} />}</For>
       </DragGroup>
       <AxisGroup />
