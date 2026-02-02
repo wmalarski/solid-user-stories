21fb67b6efde677561a053b4823069a21c2c8da1
Wojtek Malarski <w.malarski@gmail.com>
2026-02-02 22:32:55 +0100

    continue on durable objects

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -1,17 +1,31 @@
 // oxlint-disable-next-line unicorn/no-abusive-eslint-disable
 // oxlint-disable-next-line import/no-default-export import/no-anonymous-default-export
 export default {
-  async fetch(request, env) {
+  fetch(request, env) {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api")) {
       return new Response(`Running in ${navigator.userAgent} ${url.pathname}!`);
     }
 
-    if (url.pathname.startsWith("/do")) {
-      const stub = env.SYNC_DURABLE_OBJECT.getByName(new URL(request.url).pathname);
-      const greeting = await stub.sayHello();
-      return new Response(greeting);
+    if (url.pathname.startsWith("/websocket")) {
+      const upgradeHeader = request.headers.get("Upgrade");
+      if (!upgradeHeader || upgradeHeader !== "websocket") {
+        return new Response("Worker expected Upgrade: websocket", {
+          status: 426,
+        });
+      }
+
+      if (request.method !== "GET") {
+        return new Response("Worker expected GET method", {
+          status: 400,
+        });
+      }
+
+      const name = url.pathname.split("/").at(-1) ?? "default";
+      const stub = env.SYNC_DURABLE_OBJECT.getByName(name);
+
+      return stub.fetch(request);
     }
 
     return env.ASSETS.fetch(request);
